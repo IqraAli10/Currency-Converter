cd04e227750390e5fb110bc0299baea99c42e29b
Iqra Ali <aiqra9786@gmail.com>
2024-08-01 18:15:05 +0500

    Iqra Ali

---
+++ main.js
@@ -0,0 +1,37 @@
+import inquirer from "inquirer";
+const currency = {
+    "USD": 1,
+    "EUR": 0.91,
+    "GBP": 0.75,
+    "JPY": 156.45,
+    "AUD": 1.49,
+    "CAD": 1.37,
+    "CNY": 7.26,
+    "INR": 74.57,
+    "PKR": 280,
+};
+let user_answer = await inquirer.prompt([
+    {
+        name: "from",
+        message: "Enter from currency",
+        type: "list",
+        choices: ["USD", "EUR", "GBP", "JPY", "AUD", "CAD", "CNY", "INR", "PKR",]
+    },
+    {
+        name: "to",
+        message: "Enter from currency",
+        type: "list",
+        choices: ["USD", "EUR", "GBP", "JPY", "AUD", "CAD", "CNY", "INR", "PKR",]
+    },
+    {
+        name: "amount",
+        message: "Enter amount",
+        type: "number",
+    }
+]);
+let fromAmount = currency[user_answer.from];
+let toAmount = currency[user_answer.to];
+let amount = user_answer.amount;
+let baseAmount = amount / fromAmount;
+let conertedAmount = baseAmount * toAmount;
+console.log(Math.round(conertedAmount));
